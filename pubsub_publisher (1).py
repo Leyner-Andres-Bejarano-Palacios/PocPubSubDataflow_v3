62f0fe2e19d4c813cf2727598c06b376f8ee1e8d
Leyner Bejarano <personaltraingcp@gmail.com>
2022-08-23 00:19:38 +0000

    this should write text files, so we just need to write avro files

---
+++ pubsub_publisher (1).py
@@ -0,0 +1,47 @@
+# requirements
+# google-cloud-pubsub==1.7.0 
+import json
+import time
+from datetime import datetime
+from random import random
+from google.auth import jwt
+from google.cloud import pubsub_v1
+
+# --- Base variables and auth path
+CREDENTIALS_PATH =r"x-oxygen-360101-04c0c83102ea.json"
+PROJECT_ID = "x-oxygen-360101"
+TOPIC_ID = "poc-falabella"
+MAX_MESSAGES = 100
+
+# --- PubSub Utils Classes
+class PubSubPublisher:
+    def __init__(self, credentials_path, project_id, topic_id):
+        credentials = jwt.Credentials.from_service_account_info(
+            json.load(open(credentials_path)),
+            audience="https://pubsub.googleapis.com/google.pubsub.v1.Publisher"
+        )
+        self.project_id = project_id
+        self.topic_id = topic_id
+        self.publisher = pubsub_v1.PublisherClient(credentials=credentials)
+        self.topic_path = self.publisher.topic_path(self.project_id, self.topic_id)
+
+    def publish(self, data: str):
+        result = self.publisher.publish(self.topic_path, data.encode("utf-8"))
+        return result
+
+
+# --- Main publishing script
+def main():
+    i = 0
+    publisher = PubSubPublisher(CREDENTIALS_PATH, PROJECT_ID, TOPIC_ID)
+    while i < MAX_MESSAGES:
+        data = {
+            "attr1": random(),
+            "msg": f"Hi-{datetime.now()}"
+        }
+        publisher.publish(json.dumps(data))
+        time.sleep(random())
+        i += 1
+
+if __name__ == "__main__":
+    main()
---
+++ test_gcs.py
@@ -0,0 +1,148 @@
+import argparse
+from datetime import datetime
+import logging
+import random
+import os
+import apache_beam as beam
+from apache_beam import DoFn, GroupByKey, io, ParDo, Pipeline, PTransform, WindowInto, WithKeys
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.transforms.window import FixedWindows
+
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"x-oxygen-360101-a0fc362da30c.json"
+
+BIGQUERY_TABLE = "x-oxygen-360101:medium.medium_test"
+BIGQUERY_SCHEMA = "timestamp:TIMESTAMP,attr1:FLOAT,msg:STRING"
+
+class GroupMessagesByFixedWindows(PTransform):
+    """A composite transform that groups Pub/Sub messages based on publish time
+    and outputs a list of tuples, each containing a message and its publish time.
+    """
+
+    def __init__(self, window_size, num_shards=5):
+        # Set window size to 60 seconds.
+        self.window_size = int(window_size * 60)
+        self.num_shards = num_shards
+
+    def expand(self, pcoll):
+        return (
+            pcoll
+            # Bind window info to each element using element timestamp (or publish time).
+            | "Window into fixed intervals"
+            >> WindowInto(FixedWindows(self.window_size))
+            | "Add timestamp to windowed elements" >> ParDo(AddTimestamp())
+            # Assign a random key to each windowed element based on the number of shards.
+            | "Add key" >> WithKeys(lambda _: random.randint(0, self.num_shards - 1))
+            # Group windowed elements by key. All the elements in the same window must fit
+            # memory for this. If not, you need to use `beam.util.BatchElements`.
+            | "Group by key" >> GroupByKey()
+        )
+
+class AddTimestamp(DoFn):
+    def process(self, element, publish_time=DoFn.TimestampParam):
+        """Processes each windowed element by extracting the message body and its
+        publish time into a tuple.
+        """
+        yield (
+            element.decode("utf-8"),
+            datetime.utcfromtimestamp(float(publish_time)).strftime(
+                "%Y-%m-%d %H:%M:%S.%f"
+            ),
+        )
+
+class WriteToGCS(DoFn):
+    def __init__(self, output_path):
+        self.output_path = output_path
+
+    def process(self, key_value, window=DoFn.WindowParam):
+        """Write messages in a batch to Google Cloud Storage."""
+
+        ts_format = "%H:%M"
+        window_start = window.start.to_utc_datetime().strftime(ts_format)
+        window_end = window.end.to_utc_datetime().strftime(ts_format)
+        shard_id, batch = key_value
+        filename = "-".join([self.output_path, window_start, window_end, str(shard_id)])
+
+        with io.gcsio.GcsIO().open(filename=filename, mode="w") as f:
+            for message_body, publish_time in batch:
+                f.write(f"{message_body},{publish_time}\n".encode("utf-8"))
+                #json.dump(message_body, f, ensure_ascii=False)
+                #f.write(json.dumps({"test":message_body.encode("utf-8")}, ensure_ascii=False))
+def run(input_subscription, output_path, output_table, window_interval_sec, window_size=1.0, num_shards=5, pipeline_args=None):
+    # Set `save_main_session` to True so DoFns can access globally imported modules.
+    options1 = PipelineOptions(
+    pipeline_args,
+    runner='DataflowRunner',
+    project='x-oxygen-360101',
+    job_name='test-yohan-seb-11',
+    temp_location='gs://temp-medium1/temp1',
+    region='us-east1',
+    service_account_email='684034867805-compute@developer.gserviceaccount.com',
+    streaming = True,
+    save_main_session= True,
+    )
+
+    with Pipeline(options=options1) as pipeline:
+        (
+            pipeline
+            # Because `timestamp_attribute` is unspecified in `ReadFromPubSub`, Beam
+            # binds the publish time returned by the Pub/Sub server for each message
+            # to the element's timestamp parameter, accessible via `DoFn.TimestampParam`.
+            # https://beam.apache.org/releases/pydoc/current/apache_beam.io.gcp.pubsub.html#apache_beam.io.gcp.pubsub.ReadFromPubSub
+            | "Read from Pub/Sub" >> io.ReadFromPubSub(subscription=input_subscription)
+            | "Window into" >> GroupMessagesByFixedWindows(window_size, num_shards)
+            | "Write to GCS" >> ParDo(WriteToGCS(output_path))
+        )
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.INFO)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--input_subscription",
+        help="The Cloud Pub/Sub topic to read from."
+        '"projects/<PROJECT_ID>/topics/<TOPIC_ID>".',
+    )
+    parser.add_argument(
+        "--window_size",
+        type=float,
+        default=1.0,
+        help="Output file's window size in minutes.",
+    )
+    parser.add_argument(
+        "--output_path",
+        help="Path of the output GCS file including the prefix.",
+    )
+    parser.add_argument(
+        "--num_shards",
+        type=int,
+        default=5,
+        help="Number of shards to use when writing windowed elements to GCS.",
+    )
+
+    parser.add_argument(
+        "--window_interval_sec",
+        default=1,
+        type=int,
+        help="Window interval in seconds for grouping incoming messages.",
+    )
+
+    parser.add_argument(
+        "--output_table",
+        #default="x-oxygen-360101:falabella.Test1", 
+        help="Output BigQuery table for results specified as: "
+        "PROJECT:DATASET.TABLE or DATASET.TABLE.",
+        default=BIGQUERY_TABLE
+    )
+
+    known_args, pipeline_args = parser.parse_known_args()
+
+    run(
+        known_args.input_subscription,
+        known_args.output_path,
+        known_args.output_table,
+        known_args.window_interval_sec,
+        known_args.window_size,
+        known_args.num_shards,
+        pipeline_args,
+    )
