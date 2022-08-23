ebd7b05f1b3c181b948ca39015003699adc812fd
Leyner Andres Bejarano Palacios <personaltraingcp@gmail.com>
2022-08-23 03:43:56 -0500

    testing overridin WriteToAvroFile

--- test_gcs.py
+++ test_gcs.py
@@ -21,154 +21,12 @@ import fastavro
 from apache_beam.io.fileio import FileSink
 from apache_beam.io.fileio import WriteToFiles
 import fastavro
+from apache_beam.io import WriteToAvro
 
 
-def _use_fastavro():
-  return sys.version_info[0] >= 3
-
-class WriteToAvro(PTransform):
-  """A ``PTransform`` for writing avro files."""
-
-  def __init__(self,
-               file_path_prefix,
-               schema,
-               codec='deflate',
-               file_name_suffix='',
-               num_shards=0,
-               shard_name_template=None,
-               mime_type='application/x-avro',
-               use_fastavro=_use_fastavro()):
-    """Initialize a WriteToAvro transform.
-
-    Args:
-      file_path_prefix: The file path to write to. The files written will begin
-        with this prefix, followed by a shard identifier (see num_shards), and
-        end in a common extension, if given by file_name_suffix. In most cases,
-        only this argument is specified and num_shards, shard_name_template, and
-        file_name_suffix use default values.
-      schema: The schema to use, as returned by avro.schema.Parse
-      codec: The codec to use for block-level compression. Any string supported
-        by the Avro specification is accepted (for example 'null').
-      file_name_suffix: Suffix for the files written.
-      num_shards: The number of files (shards) used for output. If not set, the
-        service will decide on the optimal number of shards.
-        Constraining the number of shards is likely to reduce
-        the performance of a pipeline.  Setting this value is not recommended
-        unless you require a specific number of output files.
-      shard_name_template: A template string containing placeholders for
-        the shard number and shard count. When constructing a filename for a
-        particular shard number, the upper-case letters 'S' and 'N' are
-        replaced with the 0-padded shard number and shard count respectively.
-        This argument can be '' in which case it behaves as if num_shards was
-        set to 1 and only one file will be generated. The default pattern used
-        is '-SSSSS-of-NNNNN' if None is passed as the shard_name_template.
-      mime_type: The MIME type to use for the produced files, if the filesystem
-        supports specifying MIME types.
-      use_fastavro: when set, use the `fastavro` library for IO
-
-    Returns:
-      A WriteToAvro transform usable for writing.
-    """
-    self._sink = \
-      _create_avro_sink(
-          file_path_prefix,
-          schema,
-          codec,
-          file_name_suffix,
-          num_shards,
-          shard_name_template,
-          mime_type,
-          use_fastavro
-      )
-
-  def expand(self, pcoll):
-    output = (pcoll | ParDo(ProcessUnboundedRecordsFn(self._sink)))
-    # p = pcoll.pipeline
-    # return pcoll | beam.io.iobase.Write(self._sink)
-
-def _create_avro_sink(file_path_prefix,
-                      schema,
-                      codec,
-                      file_name_suffix,
-                      num_shards,
-                      shard_name_template,
-                      mime_type,
-                      use_fastavro):
-  return \
-      _FastAvroSink(
-          file_path_prefix,
-          schema,
-          codec,
-          file_name_suffix,
-          num_shards,
-          shard_name_template,
-          mime_type
-      ) \
-      if use_fastavro \
-      else \
-      _AvroSink(
-          file_path_prefix,
-          schema,
-          codec,
-          file_name_suffix,
-          num_shards,
-          shard_name_template,
-          mime_type
-      )
-
-class _BaseAvroSink(filebasedsink.FileBasedSink):
-  """A base for a sink for avro files. """
-  def __init__(self,
-               file_path_prefix,
-               schema,
-               codec,
-               file_name_suffix,
-               num_shards,
-               shard_name_template,
-               mime_type):
-    super(_BaseAvroSink, self).__init__(
-        file_path_prefix,
-        file_name_suffix=file_name_suffix,
-        num_shards=num_shards,
-        shard_name_template=shard_name_template,
-        coder=None,
-        mime_type=mime_type,
-        # Compression happens at the block level using the supplied codec, and
-        # not at the file level.
-        compression_type=CompressionTypes.UNCOMPRESSED)
-    self._schema = schema
-    self._codec = codec
-
-  def display_data(self):
-    res = super(_BaseAvroSink, self).display_data()
-    res['codec'] = str(self._codec)
-    res['schema'] = str(self._schema)
-    return res
-
-
-class _AvroSink(_BaseAvroSink):
-  """A sink for avro files using Avro. """
-  def open(self, temp_path):
-    file_handle = super(_AvroSink, self).open(temp_path)
-    return avro.datafile.DataFileWriter(
-        file_handle, avro.io.DatumWriter(), self._schema, self._codec)
-
-  def write_record(self, writer, value):
-    writer.append(value)
-
-
-class _FastAvroSink(_BaseAvroSink):
-  """A sink for avro files using FastAvro. """
-  def open(self, temp_path):
-    file_handle = super(_FastAvroSink, self).open(temp_path)
-    return Writer(file_handle, self._schema, self._codec)
-
-  def write_record(self, writer, value):
-    writer.write(value)
-
-  def close(self, writer):
-    writer.flush()
-    writer.fo.close()
+class OverridenClass(WriteToAvro):
+    def expand(self, pcoll):
+        output = (pcoll | ParDo(ProcessUnboundedRecordsFn(self._sink)))
 
 
 class ProcessUnboundedRecordsFn(beam.DoFn):
@@ -267,7 +125,7 @@ def run(input_subscription, output_path, output_table, window_interval_sec, wind
             | "Read from Pub/Sub" >> io.ReadFromPubSub(subscription=input_subscription)
             | "Window into" >> GroupMessagesByFixedWindows(window_size, num_shards)
             | "Extract json from key value pair" >> ParDo(ExtractJsonFromKeyValuePair())
-            | "write avros" >> WriteToAvro(known_args.output_path, schema=schema)          
+            | "write avros" >> OverridenClass(known_args.output_path, schema=schema)          
 
         )
 
