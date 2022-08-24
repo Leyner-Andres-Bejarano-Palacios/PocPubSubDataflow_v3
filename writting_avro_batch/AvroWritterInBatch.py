87faaf813b079848945048981f7f488db5910bb7
Leyner Andres Bejarano Palacios <personaltraingcp@gmail.com>
2022-08-23 20:13:25 -0500

    testing the avro writter in batch before writting using ovverriden class and then the streaming pipeline #1

--- writting_avro_batch/AvroWritterInBatch.py
+++ writting_avro_batch/AvroWritterInBatch.py
@@ -7,24 +7,37 @@ from avro.io import DatumReader, DatumWriter
 from apache_beam.io import ReadFromAvro
 from apache_beam.io import WriteToAvro
  
-schema = fastavro.schema.parse_schema({
-"type": "record",
-"namespace": "AvroPubSubDemo",
-"name": "Entity",
-"fields": [
-    {"name": "attr1", "type": "float"},
-    {"name": "msg", "type": "string"}
-],
-})
-schema = avro.schema.parse(open(“parquet_file.parqet”, “rb”).read())
+
  
-avro_write = beam.Pipeline()
- content_4 = ( avro_write
- |beam.Create({'attr1': 2.2153305989805246, 'msg': 'Hi-2022-08-22 19:44:54.495819'})
- |beam.Map(lambda element: element)
- |beam.io.WriteToAvro(‘/content/output.avro’,schema=schema))
- 
-avro_write.run()
+
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    known_args, pipeline_args = parser.parse_known_args()
+    schema = fastavro.schema.parse_schema({
+    "type": "record",
+    "namespace": "AvroPubSubDemo",
+    "name": "Entity",
+    "fields": [
+        {"name": "attr1", "type": "float"},
+        {"name": "msg", "type": "string"}
+    ],
+    })
+    options1 = PipelineOptions(
+    argv=pipeline_args,
+    runner='DataflowRunner',
+    project='x-oxygen-360101',
+    job_name='yohan-lule-dantodomingo-1',
+    temp_location='gs://temp-medium1/temp1',
+    region='us-east1',
+    service_account_email='684034867805-compute@developer.gserviceaccount.com',
+    save_main_session= True)
+    #schema = avro.schema.parse(open(“parquet_file.parqet”, “rb”).read())
     logging.getLogger().setLevel(logging.DEBUG)
+    avro_write = beam.Pipeline(options=options1)
+    content_4 = ( avro_write
+    |beam.Create({'attr1': 2.2153305989805246, 'msg': 'Hi-2022-08-22 19:44:54.495819'})
+    |beam.Map(lambda element: element)
+    |beam.io.WriteToAvro('gs://test-carga1/test/output.avro',schema=schema))
+
+    avro_write.run()
