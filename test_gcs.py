de199eb51f688767f8708b3b6eeecc6992bb6305
Leyner Andres Bejarano Palacios <personaltraingcp@gmail.com>
2022-08-24 22:30:06 -0500

    deleting commented code, testing branching function and writting to bigQuery v7

--- test_gcs.py
+++ test_gcs.py
@@ -264,7 +264,7 @@ def run(input_subscription, output_path, output_table, window_interval_sec, wind
                 | "Read from Pub/Sub" >> io.ReadFromPubSub(subscription=input_subscription)
                 # | "Window into" >> GroupMessagesByFixedWindows(window_size, num_shards)
                 | "Fixed-size windows" >> beam.WindowInto(window.FixedWindows(window_interval_sec, 0))
-                | "Extract json from key value pair" >> ParDo(ExtractJsonFromKeyValuePair())
+                # | "Extract json from key value pair" >> ParDo(ExtractJsonFromKeyValuePair())
                 |  beam.ParDo(fn_check_schema()).with_outputs()
             )
 
